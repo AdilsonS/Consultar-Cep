4c1bbad36f573b64df6eff9d6eac62b25d6b66c4
Adilson Souza <adilsonsouzadocarmojf@hotmail.com>
2019-11-05 22:28:07 -0300

    Iniciado projeto

---
+++ ConsultaCPF/ConsultaCPF/Servico/ViaCEPServico.cs
@@ -0,0 +1,23 @@
+﻿using ConsultaCPF.Servico.Modelo;
+using Newtonsoft.Json;
+using System.Net;
+
+namespace ConsultaCPF.Servico
+{
+  public class ViaCEPServico
+  {
+    private static string url = "http://viacep.com.br/ws/{0}/json/";
+
+    public static Endereco BuscarEndercoViaCEP(string cep)
+    {
+      string urlCompleta = string.Format(url, cep);
+
+      WebClient cliente = new WebClient();
+      string retorno = cliente.DownloadString(urlCompleta);
+
+      var enderecoRetornado = JsonConvert.DeserializeObject<Endereco>(retorno);
+
+      return enderecoRetornado;
+    }
+  }
+}
